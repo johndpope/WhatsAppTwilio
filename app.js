d7dd342c224984a2841765ae0497e2bc918f47b5
ACERPORTATIL\yogal <yogalnuri@gmail.com>
2019-05-21 23:11:03 +0200

    Start based on chatbot

---
+++ app.js
@@ -0,0 +1,113 @@
+/*var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+app.set('port', process.env.PORT || 5000);
+
+var app = express();
+var apiai = require('apiai');
+var apiapp = apiai("6b53faafccee40c881ee6058f8cee748");
+
+//Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+  app.post(req,res);
+});
+
+app.post('/', function(req, res){
+  console.log(req.body.chatText);
+  var request = apiapp.textRequest(req.body.chatText, {
+    sessionId: '123123'
+  });
+  request.on('response', function(response) {
+      console.log(response);
+      res.send(response);
+  });
+  request.on('error', function(error) {
+      console.log(error);
+      res.send(error);
+  });
+  request.end();
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});*/
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+/*app.post('/update', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});*/
+/*app.get('/update', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});*/
+app.post('/chatConnect', function(req,res){});
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
