dd701a654774a5670d4e150dce8d48f43f309b80
CTS\733259 <for Eloy.AlfagemeGaleano@cognizant.com>
2019-05-23 15:07:21 +0200

    no message

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 var accountSid = 'AC67968749065aa4c24a598fb476e3ee1e'; // Your Account SID from www.twilio.com/console
 var authToken = 'b1993d51e0dfeff9bf683e82b870f5e8'; // Your Auth Token from www.twilio.com/console
 app.set('port', process.env.PORT || 5000);
-console.log("Pureto de salida");
+console.log("Puerto de salida");
 const client = require('twilio')(accountSid, authToken);
 console.log("Twiliooooo");
 client.messages
@@ -36,8 +36,13 @@ client.messages
 })
 .then((message) => console.log(message.sid));*/
 
-/*app.post('/update', function(req, res) {
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+app.post('/update', function(req, res) {
+    console.log("-----------------------------------------------");
+    console.log(req);
+    console.log("-----------------------------------------------");
+    console.log(res);
+    console.log("-----------------------------------------------");
+    /*pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
@@ -65,8 +70,8 @@ client.messages
                 }
             }
         );
-    });
-});
+    });*/
+});/*
 app.post("/chatbotJoin", function(req, res){
 
     console.log("My request-->"+req.body.firstName);
