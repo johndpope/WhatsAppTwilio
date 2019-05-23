59b4c126976f899ba378be38571b286cb2a5f6ac
CTS\733259 <for Eloy.AlfagemeGaleano@cognizant.com>
2019-05-23 17:27:14 +0200

    no message

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/', (req, res) => {
     const twiml = new MessagingResponse();
     twiml.message('Helloo'); 
     console.log("---------------------------------------------------------");
-    /*console.log(req.body);*/
+    console.log(req.body);
     console.log("SmsMessageSid "+req.body.Body.SmsMessageSid);
     console.log("From:" +req.body.Body.From);
     console.log("MessageSid "+req.body.Body.MessageSid);
@@ -47,6 +47,22 @@ app.post('/', (req, res) => {
     }else{
         twiml.message(':(');
     }
+    console.log("---------------------");
+    req.on('end', function() {
+        var data = qs.parse(body);
+        var jsonString = json.stringify(data);  
+        var jsonDataObject = json.parse(jsonString);
+        console.log("SmsMessageSid "+jsonDataObject.Body.SmsMessageSid);
+        console.log("From:" +jsonDataObject.Body.From);
+        console.log("MessageSid "+jsonDataObject.Body.MessageSid);
+        var message = jsonDataObject.Body.body;
+        console.log("Message" + message);
+        if (message == ":)"){
+            twiml.message(':)');    
+        }else{
+            twiml.message(':(');
+        }
+      });
     console.log("-----------------------------------------------------------");
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
