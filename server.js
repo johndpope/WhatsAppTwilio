a78fc3cabfd4079792c74a4a2c96b2bbd0276845
ACERPORTATIL\yogal <yogalnuri@gmail.com>
2019-05-26 12:46:14 +0200

    no message

--- server.js
+++ server.js
@@ -19,19 +19,41 @@ app.use(bodyParser.json());
 
 var accountSid = 'AC67968749065aa4c24a598fb476e3ee1e'; // Your Account SID from www.twilio.com/console
 var authToken = 'b1993d51e0dfeff9bf683e82b870f5e8'; // Your Auth Token from www.twilio.com/console
-app.set('port', process.env.PORT || 5000);
+
 const client = require('twilio')(accountSid, authToken);
 
 
 
-console.log("Twilio");
-client.messages.create({
-        from: 'whatsapp:+14155238886',
-        body: 'Hello there!',
-        to: 'whatsapp:+34689603272'
-    })
-    .then(message => console.log(message.sid + "  ----> " +message.body));
+console.log("Twilionn");
 
+function intervalFunc() {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'Select SA.Subject, U.MobilePhone from ServiceAppointment SA left join AssignedResource AR ON  SA.Id=AR.ServiceAppointmentId left join ServiceResource SR ON AR.ServiceResourceId=SR.Id left join User U on SR.RelatedRecordId= U.Id Where SA.WhatsApp_Sent__c=false',
+            function(err, result) {
+               
+                if (err != null || result.rowCount == 0) {
+                    
+                }
+                else {
+                    var resultador=response.status(200).json(results.rows)
+                    console.log(resultador);
+                   /* client.messages.create({
+                        from: 'whatsapp:+14155238886',
+                        body: 'Hello there!',
+                        to: 'whatsapp:+34626561876'
+                    })
+                    .then(message => console.log(message.sid + "  ----> " +message.body));*/
+                }
+            }
+        );
+    });
+}
+    
+setInterval(intervalFunc, 1500);
+app.set('port', process.env.PORT || 5000);
 app.post('/', (req, res) => {
     const twiml = new MessagingResponse();
     twiml.message('Helloo you say: '); 
