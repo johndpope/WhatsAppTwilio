938eb8f13bba1b50f14f1b845dba8aa11db424ce
ACERPORTATIL\yogal <yogalnuri@gmail.com>
2019-05-30 01:14:44 +0200

    no message

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.post('/', (req, res) => {
                                 res.end(twiml.toString());
                             }
                             else {
-                                console.log('Result Insert-->'+result);
+                                console.log('Result Insert-->'+result.rows);
                                 const twiml = new MessagingResponse();
                                 twiml.message('Case created');    
                                 res.writeHead(200, {'Content-Type': 'text/xml'});
